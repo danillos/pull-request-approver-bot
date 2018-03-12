5049bcba0365c6b3231b812d160becd67ff9535b
Danillo Melo <danillos@gmail.com>
2018-03-11 21:07:26 -0300

    Fix sort by

--- lib/github.ex
+++ lib/github.ex
@@ -52,9 +52,9 @@ defmodule Github do
 
   def last_reviews_by_user_for_issue(issue) do
     reviews_for_issue(issue)
-    |> Enum.filter(fn(x)   -> (x["state"] == "COMMENTED" && x["body"] != "" && x["user"]["login"] != issue["user"]["login"]) || x["state"] != "COMMENTED" end)
-    |> Enum.group_by(fn(i) -> i["user"]["login"] end)
-    |> Enum.map(fn {_k, v} -> Enum.sort_by(v, fn(d) -> Timex.parse(d, "{ISO:Extended}") end) |>  List.last  end)
+    |> Enum.filter(fn(review) -> valid_review?(review, issue) end)
+    |> Enum.group_by(fn(review) -> review["user"]["login"] end)
+    |> Enum.map(fn {_k, reviews} -> last_review(reviews) end)
   end
 
   def requested_reviewers_for_issue(issue) do
@@ -63,6 +63,25 @@ defmodule Github do
 
   def access_token do
   	Application.get_env(:github_approver, :github_access_token)
+  end
+
+  defp last_review(reviews) do
+    Enum.sort_by(reviews, fn(review) -> Timex.parse(review["submitted_at"], "{ISO:Extended}") end) |>  List.last
+  end
+
+  defp valid_review?(review, issue) do
+    regular_review?(review) || valid_review_comment?(review, issue)
+  end
+
+  defp valid_review_comment?(review, issue) do
+    empty_body = review["body"] == ""
+    owner_comment = review["user"]["login"] == issue["user"]["login"]
+
+    !regular_review?(review) && !empty_body && !owner_comment
+  end
+
+  defp regular_review?(review) do
+    review["state"] != "COMMENTED"
   end
 
   defp label_names(issue) do
